6913a3bf925a8a175cab541c107ff5e3718926c1
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-16 13:45:08 +0530

    added new components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
+import About from "@/components/layout/About";
+import Contact from "@/components/layout/Contact";
+import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
+import Hero from "@/components/layout/Hero";
+import HomeMenu from "@/components/layout/HomeMenu";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
+      <HomeMenu />
+      <About />
+      <Contact />
+      <Footer />
     </>
   );
 }
