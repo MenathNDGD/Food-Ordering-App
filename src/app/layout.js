cd1fe9639bf0a5c3f5b45f6e543fdc21d692e73a
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-18 22:36:53 +0530

    added the app provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+import { AppProvider } from "@/components/AppContext"
 
 const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500', '700'] });
 
@@ -13,7 +16,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto p-4">
-          {children}
+          <AppProvider>
+            <Header />
+            {children}
+            <Footer />
+          </AppProvider>
         </main>
       </body>
     </html>
