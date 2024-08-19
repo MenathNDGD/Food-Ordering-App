42bfb56420c61e0dea6d846c3f14b3bab4d50071
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-19 23:36:16 +0530

    modified the app route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,7 +6,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import client from "@/libs/mongoConnect";
 
-const handler = NextAuth({
+export const authOptions = {
   secret: process.env.SECRET,
   adapter: MongoDBAdapter(client),
   providers: [
@@ -36,7 +36,9 @@ const handler = NextAuth({
         return null
       }
     })
-  ]
-})
+  ],
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
