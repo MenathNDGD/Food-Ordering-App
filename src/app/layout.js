034b7100efce3a84ae14c0f7217a2bb512769dcf
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-20 23:16:27 +0530

    added the toaster

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import { AppProvider } from "@/components/AppContext"
+import { Toaster } from "react-hot-toast";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500', '700'] });
 
@@ -15,8 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <main className="max-w-4xl mx-auto p-4">
+        <main className="max-w-4xl p-4 mx-auto">
           <AppProvider>
+            <Toaster />
             <Header />
             {children}
             <Footer />
