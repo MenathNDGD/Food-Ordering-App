d007a53b1984ec5507b172e6517c2d533bce732e
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-20 23:26:50 +0530

    created the user tabs component

---
+++ src/components/layout/UserTabs.js
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function UserTabs({ isAdmin }) {
+  const path = usePathname();
+  return (
+    <div className="flex justify-center gap-2 mx-auto tabs">
+      <Link className={path === "/profile" ? "active" : ""} href={"/profile"}>
+        Profile
+      </Link>
+      {isAdmin && (
+        <>
+          <Link
+            className={path === "/categories" ? "active" : ""}
+            href={"/categories"}
+          >
+            Categories
+          </Link>
+          <Link
+            className={path.includes('menu-items') ? "active" : ""}
+            href={"/menu-items"}
+          >
+            Menu Items
+          </Link>
+          <Link
+            className={path === "/users" ? "active" : ""}
+            href={"/users"}
+          >
+            Users
+          </Link>
+        </>
+      )}
+    </div>
+  );
+}
