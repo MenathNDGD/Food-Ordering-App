6c2e1de162be221377258acd7d67f49c207dcb11
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-21 22:37:42 +0530

    modified the categories page with new buttons and styles

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import DeleteButton from "@/components/DeleteButton";
 import UserTabs from "@/components/layout/UserTabs";
 import { useProfile } from "@/components/UseProfile";
 import { useEffect, useState } from "react";
@@ -50,6 +51,27 @@ export default function CategoriesPage() {
             error: 'Category Creating Failed!',
         });
     }
+
+    async function handleDeleteClick(_id) {
+        const promise = new Promise(async (resolve, reject) => {
+            const response = await fetch('/api/categories?_id' + _id, {
+                method: 'DELETE',
+            });
+            if (response.ok) {
+                resolve();
+            } else {
+                reject();
+            }
+        });
+        
+        await toast.promise(promise, {
+            loading: 'Category Deleting...',
+            success: 'Category Deleted!',
+            error: 'Deleting Failed!',
+        });
+
+        fetchCategories();
+    }
     
     if (profileLoading) {
         return 'Loading User Info...';
@@ -60,7 +82,7 @@ export default function CategoriesPage() {
     }
    
     return (
-        <section className="max-w-md mx-auto mt-8">
+        <section className="max-w-2xl mx-auto mt-8">
             <UserTabs isAdmin={true} />
             <form className="mt-8" onSubmit={handleCategorySubmit}>
                 <div className="flex items-end gap-2">
@@ -77,28 +99,47 @@ export default function CategoriesPage() {
                             onChange={ev => setCategoryName(ev.target.value)}
                         />
                     </div>
-                    <div className="pb-4">
+                    <div className="flex gap-2 pb-4">
                         <button
                             className="border border-primary" 
                             type="submit"
                         >
                             {editedCategory ? 'Update' : 'Create'}
                         </button>
+                        <button
+                            type="button"
+                            onClick={() => {
+                                setEditedCategory(null);
+                                setCategoryName('');
+                            }}
+                        >
+                            Cancel
+                        </button>
                     </div>
                 </div>
             </form>
             <div>
-                <h2 className="mt-8 text-sm text-gray-500">Edit Category</h2>
+                <h2 className="mt-8 text-sm text-gray-500">Existing Categories</h2>
                 {categories?.length > 0 && categories.map(c => (
-                    <button
-                        onClick={() => {
-                            setEditedCategory(c);
-                            setCategoryName(c.name);
-                        }} 
-                        className="flex gap-1 p-2 px-4 mb-1 cursor-pointer rounded-xl"
+                    <div 
+                        className="flex items-center gap-1 p-2 px-4 mb-1 bg-gray-100 rounded-xl"
                     >
-                        <span>{c.name}</span>
-                    </button>
+                        <div className="grow">
+                            {c.name}
+                        </div>
+                        <div className="flex gap-2">
+                            <button 
+                                type="button"
+                                onClick={() => {
+                                    setEditedCategory(c);
+                                    setCategoryName(c.name);
+                                }}
+                            >
+                                Edit
+                            </button>
+                            <DeleteButton label="Delete" onDelete={() => handleDeleteClick(c._id)} />
+                        </div>
+                    </div>
                 ))}
             </div>
         </section>
