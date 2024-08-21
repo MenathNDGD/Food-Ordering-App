5a4a0ab6bc7019955728b4950535527d5977bc43
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-21 22:38:36 +0530

    created the menu items edit page

--- src/app/menu-items/edit/[id]/page.js
+++ src/app/menu-items/edit/[id]/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import DeleteButton from "@/components/DeleteButton";
 import LeftArrow from "@/components/icons/LeftArrow";
 import MenuItemForm from "@/components/layout/MenuItemForm";
 import UserTabs from "@/components/layout/UserTabs";
@@ -46,6 +47,27 @@ export default function EditMenuItemPage() {
             success: 'Item Updated!',
             error: 'Updating Failed!',
         });
+        
+        setRedirectToItems(true);
+    }
+
+    async function handleDeleteClick() {
+        const promise = new Promise(async (resolve, reject) => {
+            const res = await fetch('/api/menu-items?_id=' + id, {
+                method: 'DELETE',
+            });
+            if (res.ok)
+                resolve();
+            else
+                reject();
+        });
+
+        await toast.promise(promise, {
+            loading: 'Deleting Menu Item...',
+            success: 'Menu Item Deleted!',
+            error: 'Deleting Failed!',
+        });
+
         setRedirectToItems(true);
     }
     
@@ -63,13 +85,18 @@ export default function EditMenuItemPage() {
     return (
         <section className="mt-8">
             <UserTabs isAdmin={true} />
-            <div className="max-w-md mx-auto mt-8">
+            <div className="max-w-2xl mx-auto mt-8">
                 <Link href={'/menu-items'} className="button">
                     <LeftArrow />
                     <span>Show All Menu Items</span>
                 </Link>
             </div>
             <MenuItemForm menuItem={menuItem} onSubmit={handleFormSubmit} />
+            <div className="max-w-md mx-auto mt-2">
+                <div className="max-w-xs pl-4 ml-auto">
+                    <DeleteButton label="Delete Menu Item" onDelete={handleDeleteClick} />
+                </div>
+            </div>
         </section>
     );
 }
