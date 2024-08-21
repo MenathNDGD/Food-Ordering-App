e47188a92c3ee10a2863a5e62ee08b934b3cf732
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-21 22:43:58 +0530

    modified the user profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import EditableImage from "@/components/layout/EditableImage";
 import UserTabs from "@/components/layout/UserTabs";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
@@ -9,13 +8,7 @@ import toast from "react-hot-toast";
 
 export default function ProfilePage() {
   const session = useSession();
-  const [userName, setUserName] = useState("");
-  const [image, setImage] = useState("");
-  const [phone, setPhone] = useState("");
-  const [streetAddress, setStreetAddress] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [city, setCity] = useState("");
-  const [country, setCountry] = useState("");
+  
   const [isAdmin, setIsAdmin] = useState(false);
   const [profileFetched, setProfileFetched] = useState(false);
   const { status } = session;
@@ -77,71 +70,8 @@ export default function ProfilePage() {
   return (
     <section className="mt-8">
       <UserTabs isAdmin={isAdmin} />
-      <div className="max-w-md mx-auto mt-8">
-        <div className="flex gap-4">
-          <div>
-            <div className="relative p-2 bg-gray-300 rounded-lg max-w-[120px]">
-              <EditableImage link={image} setLink={setImage} />
-            </div>
-          </div>
-          <form className="grow" onSubmit={handleProfileInfoUpdate}>
-            <label>First & Last Name</label>
-            <input
-              type="text"
-              placeholder="First & Last Name"
-              value={userName}
-              onChange={(ev) => setUserName(ev.target.value)}
-            />
-            <label>Email</label>
-            <input
-              type="email"
-              value={"session.data.user.email"}
-              disabled={true}
-            />
-            <label>Phone</label>
-            <input
-              type="tel"
-              placeholder="Phone Number"
-              value={phone}
-              onChange={(ev) => setPhone(ev.target.value)}
-            />
-            <label>Street Address</label>
-            <input
-              type="text"
-              placeholder="Street Address"
-              value={streetAddress}
-              onChange={(ev) => setStreetAddress(ev.target.value)}
-            />
-            <div className="flex gap-2">
-              <div>
-                <label>Postal Code</label>
-                <input
-                  type="text"
-                  placeholder="Postal Code"
-                  value={postalCode}
-                  onChange={(ev) => setPostalCode(ev.target.value)}
-                />
-              </div>
-              <div>
-                <label>City</label>
-                <input
-                  type="text"
-                  placeholder="City"
-                  value={city}
-                  onChange={(ev) => setCity(ev.target.value)}
-                />
-              </div>
-            </div>
-            <label>Country</label>
-            <input
-              type="text"
-              placeholder="Country"
-              value={country}
-              onChange={(ev) => setCountry(ev.target.value)}
-            />
-            <button type="submit">Save</button>
-          </form>
-        </div>
+      <div className="max-w-2xl mx-auto mt-8">
+        
       </div>
     </section>
   );
