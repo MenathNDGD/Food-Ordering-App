881295ac9018079eca3d781b429d779fe3d72f5d
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-22 23:58:35 +0530

    modified the route for the user profile

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -5,30 +5,36 @@ import { User } from "@/models/User";
 import { UserInfo } from "@/models/UserInfo";
 
 export async function PUT(req) {
-    mongoose.connect(process.env.MONGO_URL);
-    const data = await req.json();
-    const {name, image, ...otherUserInfo} = data;
+  mongoose.connect(process.env.MONGO_URL);
+  const data = await req.json();
+  const { _id, name, image, ...otherUserInfo } = data;
 
+  let filter = {};
+  if (_id) {
+    filter = { _id };
+  } else {
     const session = await getServerSession(authOptions);
     const email = session.user.email;
+    filter = { email };
+  }
 
-    await User.updateOne({email}, {name, image});
+  const user = await User.findOne(filter);
+  await User.updateOne(filter, { name, image });
+  await UserInfo.findOneAndUpdate({email: user.email}, otherUserInfo, { upsert: true });
 
-    await UserInfo.findOneAndUpdate({email}, otherUserInfo, {upsert: true});
-
-    return Response.json(true);
+  return Response.json(true);
 }
 
 export async function GET() {
-    mongoose.connect(process.env.MONGO_URL);
-    const session = await getServerSession(authOptions);
-    const email = session?.user?.email;
+  mongoose.connect(process.env.MONGO_URL);
+  const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
 
-    if (!email) {
-        return Response.json({});
-    }
+  if (!email) {
+    return Response.json({});
+  }
 
-    const user = await User.findOne({email}).lean();
-    const userInfo = await UserInfo.findOne({email}).lean();
-    return Response.json({...user, ...userInfo});
+  const user = await User.findOne({ email }).lean();
+  const userInfo = await UserInfo.findOne({ email }).lean();
+  return Response.json({ ...user, ...userInfo });
 }
