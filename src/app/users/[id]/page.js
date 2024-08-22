a428706bfe4bc2e072df7fdd727628f9addfbc26
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-23 00:01:19 +0530

    modified the user fetch data function

--- src/app/users/[id]/page.js
+++ src/app/users/[id]/page.js
@@ -1,24 +1,59 @@
 "use client";
 
+import UserForm from "@/components/layout/UserForm";
 import UserTabs from "@/components/layout/UserTabs";
 import { useProfile } from "@/components/UseProfile";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function EditUserPage() {
-    const {loading, data} = useProfile();
-    
-    if (loading) {
-        return 'Loading User Info...';
-    }
-    if (!data.admin) {
-        return 'Not an Admin';
-    }
-    
-    return (
-        <section className="max-w-2xl mx-auto mt-8">
-            <UserTabs isAdmin={true} />
-            <div className="mt-8">
-                User Info Form
-            </div>
-        </section>
-    );
+  const { loading, data } = useProfile();
+  const [user, setUser] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch("/api/profile?_id=" + id).then((res) => {
+      res.json().then((user) => {
+        setUser(user);
+      });
+    });
+  }, []);
+
+  async function handleSaveButtonClick(ev, data) {
+    ev.preventDefault();
+    const promise = new Promise(async (resolve, reject) => {
+      const res = await fetch("/api/profile", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ...data, _id: id }),
+      });
+      if (res.ok) 
+        resolve();
+      else 
+        reject();
+    });
+
+    await toast.promise(promise, {
+        loading: 'Saving User...',
+        success: 'User Saved!',
+        error: 'Saving Failed!',
+    });
+  }
+
+  if (loading) {
+    return "Loading User Info...";
+  }
+  if (!data.admin) {
+    return "Not an Admin";
+  }
+
+  return (
+    <section className="max-w-2xl mx-auto mt-8">
+      <UserTabs isAdmin={true} />
+      <div className="mt-8">
+        <UserForm user={user} onSave={handleSaveButtonClick} />
+      </div>
+    </section>
+  );
 }
